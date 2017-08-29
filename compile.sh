c5dd35724bfb963ed27527e58658f350356f63e8
Don Schenck <don.schenck@gmail.com>
2017-08-29 16:46:29 -0400

    Fixed so Travis wouldn't keep breaking

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-pandoc ./Compass.md  -f markdown -t html -s -o out/index.html
-pandoc ./glossary.md -f markdown -t html -s -o out/glossary.html
+pandoc ./Compass.md  -f markdown -t html -s -o docs/index.html
+pandoc ./glossary.md -f markdown -t html -s -o docs/glossary.html
