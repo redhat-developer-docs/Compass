8fe73cf39b1569da0debef881b6b0b86a33d58a8
Don Schenck <don.schenck@gmail.com>
2017-08-29 13:26:08 -0400

    added local compile script

---
+++ compile.ps1
@@ -0,0 +1,2 @@
+pandoc ./Compass.md  -f markdown -t html -s -o docs/index.html
+pandoc ./glossary.md -f markdown -t html -s -o docs/glossary.html
---
+++ compile.sh
@@ -0,0 +1,2 @@
+pandoc ./Compass.md  -f markdown -t html -s -o out/index.html
+pandoc ./glossary.md -f markdown -t html -s -o out/glossary.html
